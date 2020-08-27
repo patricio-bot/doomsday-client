1cc9cda9accf26155b7246b9acca09f8bc4add31
Patricio Castro <luckydrawings@gmail.com>
2020-08-27 11:08:22 +0200

    fixed css

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -14,14 +14,17 @@ export default function UserCard(props) {
             </div>
             <div className="card-box card-box-content">
                 <Link to={`/user/${_id}`}><h3>{firstName} {lastName}</h3></Link>
-                <p>{gender}</p>
-                <div>{tasksCreated.map(task => {
+                <p>Gender: {gender}</p>
+                {/*   <div>{tasksCreated.map(task => {
                     return <p key={task._id}>{task.title} {task.description}</p>
-                })}</div>
-                <p>{yearsRemaining}</p>
-                <p>{health}</p>
-                <p>{weight}</p>
-                <p>{height}</p>
+                })}</div> */}
+
+                <div className="capitalize">
+                    <p>Health: {health}</p>
+                    <p>Weight: {weight} kg</p>
+                    <p>Height: {height} cms</p>
+                </div>
+
 
             </div>
         </div>
